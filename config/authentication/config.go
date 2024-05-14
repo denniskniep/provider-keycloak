2a47d36ffc2814921b234c7e2f634a13eb99153e
Isabella Dix <isabella.dix@merckgroup.com>
2024-05-14 14:30:07 +0200

    Test commit

---
+++ config/authentication/config.go
@@ -0,0 +1,42 @@
+package authentication
+
+import "github.com/crossplane/upjet/pkg/config"
+
+const (
+	// Group is the short group for this provider.
+	Group = "authenticationflow"
+)
+
+// Configure configures individual resources by adding custom ResourceConfigurators.
+func Configure(p *config.Provider) {
+	p.AddResourceConfigurator("keycloak_authentication_flow", func(r *config.Resource) {
+		r.ShortGroup = Group
+		r.References["realm_id"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
+		}
+	})
+	p.AddResourceConfigurator("keycloak_authentication_subflow", func(r *config.Resource) {
+		r.ShortGroup = Group
+		r.References["realm_id"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
+		}
+	})
+	p.AddResourceConfigurator("keycloak_authentication_execution", func(r *config.Resource) {
+		r.ShortGroup = Group
+		r.References["realm_id"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
+		}
+	})
+	p.AddResourceConfigurator("keycloak_authentication_execution_config", func(r *config.Resource) {
+		r.ShortGroup = Group
+		r.References["realm_id"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
+		}
+	})
+	p.AddResourceConfigurator("keycloak_authentication_bindings", func(r *config.Resource) {
+		r.ShortGroup = Group
+		r.References["realm_id"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
+		}
+	})
+}

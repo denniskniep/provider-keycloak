7075ef60efe7c9bec33fb9f042f880fc160e5b63
Isabella Dix <isabella.dix@merckgroup.com>
2024-05-29 12:53:25 +0200

    Remove unnecessary realm_id references and add keycloak_ldap_msad_user_account_control_mapper

--- config/authentication/config.go
+++ config/authentication/config.go
@@ -14,15 +14,9 @@ const (
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("keycloak_authentication_flow", func(r *config.Resource) {
 		r.ShortGroup = Group
-		r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
-		}
 	})
 	p.AddResourceConfigurator("keycloak_authentication_subflow", func(r *config.Resource) {
 		r.ShortGroup = Group
-		/*r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
-		}*/
 		r.References["parent_flow_alias"] = config.Reference{
 			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
 			Extractor:         common.PathAuthenticationFlowAliasExtractor,
@@ -32,55 +26,56 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("keycloak_authentication_execution", func(r *config.Resource) {
 		r.ShortGroup = Group
-		r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
-		}
 		r.References["parent_flow_alias"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "ParentFlowAliasRef",
+			SelectorFieldName: "ParentFlowAliasSelector",
 		}
 	})
 	p.AddResourceConfigurator("keycloak_authentication_execution_config", func(r *config.Resource) {
 		r.ShortGroup = Group
-		r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
-		}
-		r.References["parent_flow_alias"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
-		}
 		r.References["execution_id"] = config.Reference{
 			Type: "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Execution",
 		}
 	})
 	p.AddResourceConfigurator("keycloak_authentication_bindings", func(r *config.Resource) {
 		r.ShortGroup = Group
-		r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
-		}
 		r.References["browser_flow"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "BrowserFlowRef",
+			SelectorFieldName: "BrowserFlowSelector",
 		}
 		r.References["registration_flow"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "RegistrationFlowRef",
+			SelectorFieldName: "RegistrationFlowSelector",
 		}
 		r.References["direct_grant_flow"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "DirectGrantFlowRef",
+			SelectorFieldName: "DirectGrantFlowSelector",
 		}
 		r.References["reset_credentials_flow"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "ResetCredentialsFlowRef",
+			SelectorFieldName: "ResetCredentialsFlowSelector",
 		}
 		r.References["client_authentication_flow"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "ClientAuthenticationFlowRef",
+			SelectorFieldName: "ClientAuthenticationFlowSelector",
 		}
 		r.References["docker_authentication_flow"] = config.Reference{
-			Type:      "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
-			Extractor: common.PathAuthenticationFlowAliasExtractor,
+			Type:              "github.com/crossplane-contrib/provider-keycloak/apis/authenticationflow/v1alpha1.Flow",
+			Extractor:         common.PathAuthenticationFlowAliasExtractor,
+			RefFieldName:      "DockerAuthenticationFlowRef",
+			SelectorFieldName: "DockerAuthenticationFlowSelector",
 		}
 	})
 }
--- config/ldapuserfederation/config.go
+++ config/ldapuserfederation/config.go
@@ -11,15 +11,15 @@ const (
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("keycloak_ldap_user_federation", func(r *config.Resource) {
 		r.ShortGroup = Group
-		r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
-		}
 	})
 	p.AddResourceConfigurator("keycloak_ldap_user_attribute_mapper", func(r *config.Resource) {
 		r.ShortGroup = Group
-		r.References["realm_id"] = config.Reference{
-			Type: "github.com/crossplane-contrib/provider-keycloak/apis/realm/v1alpha1.Realm",
+		r.References["ldap_user_federation_id"] = config.Reference{
+			Type: "github.com/crossplane-contrib/provider-keycloak/apis/ldapuserfederation/v1alpha1.UserFederation",
 		}
+	})
+	p.AddResourceConfigurator("keycloak_ldap_msad_user_account_control_mapper", func(r *config.Resource) {
+		r.ShortGroup = Group
 		r.References["ldap_user_federation_id"] = config.Reference{
 			Type: "github.com/crossplane-contrib/provider-keycloak/apis/ldapuserfederation/v1alpha1.UserFederation",
 		}
